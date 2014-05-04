4f475722d7cfed61513c9a332d380ecb694248f0
JonMuehlst <jon.muehlst@gmail.com>
2014-05-05 00:50:03 +0300

    fixed main. wrong fork implementation

--- source/shell.c
+++ source/shell.c
@@ -28,6 +28,8 @@ int main(int argc, char *argv[]){
   char lineBCKP[1024];
   int n = 0; /* number of calls to 'command' */
   //int i = 0;
+  pid_t pid = 0;
+  
   while(1) {
     //check_all_errorcheck();
     //printf("\n");
@@ -41,7 +43,6 @@ int main(int argc, char *argv[]){
       break;
     } 
     
-    printf("%s\n", line);
     /* Remove the trailing newline 
     line[strlen(line) - 1] = '\0'; */
     
@@ -56,40 +57,34 @@ int main(int argc, char *argv[]){
     int trailingAmp = 0;
     int status = 0;
     int delimitFlag = 0;
-    pid_t pid = 0;
+    
     char* cmd = line;
     char* next = strpbrk(cmd, DELIMITERS); /* Find first '|' or '&' */
     
-    pid = fork();
     
-    if(pid == 0){  
-	while (next != NULL) {
-		/* 'next' points to '|' or '&' */
-		if(*next == '|') 
-		  delimitFlag = PIPE_NUM;
-		if(*next == '&'){ 
-		  delimitFlag = AMPERSAND_NUM;
-		  if(*(next + 1) == '\n'){ 
-		    trailingAmp = 1;
-		    //printf("trailing amp\n");
-		  }
-		}
-		//if(*next == '\n') delimitFlag = NEWLINE_NUM;
-		*next = '\0';
-		input = run(cmd, input, first, 0, &n, delimitFlag);
-		cmd = next + 1;
-		next = strpbrk(cmd, DELIMITERS); /* Find next '|' or '&' */
-		first = 0;
-	}
-	if(!trailingAmp)
-	  input = run(cmd, input, first, 1, &n);
-	cleanup(n);
-	n = 0;
-    } else if(lineBCKP[strlen(lineBCKP) - 2] == '&') {
-	waitpid(pid, &status, WNOHANG);
-    } else {
-	wait(pid, &status, 0);
+    while (next != NULL) {
+	    /* 'next' points to '|' or '&' */
+	    if(*next == '|') 
+	      delimitFlag = PIPE_NUM;
+	    if(*next == '&'){ 
+	      delimitFlag = AMPERSAND_NUM;
+	      if(*(next + 1) == '\n'){ 
+		trailingAmp = 1;
+		//printf("trailing amp\n");
+	      }
+	    }
+	    //if(*next == '\n') delimitFlag = NEWLINE_NUM;
+	    *next = '\0';
+	    input = run(cmd, input, first, 0, &n, delimitFlag);
+	    cmd = next + 1;
+	    next = strpbrk(cmd, DELIMITERS); /* Find next '|' or '&' */
+	    first = 0;
     }
+    if(!trailingAmp)
+      input = run(cmd, input, first, 1, &n);
+    cleanup(n);
+    n = 0;
+    
       
   }
   
